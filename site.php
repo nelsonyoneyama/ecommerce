c34d75416c2f90a61438f28fd689763b69c96bb4
nyoneyama <nelsonyoneyama@hotmail.com>
2020-05-31 10:25:58 -0300

    Paginação

--- site.php
+++ site.php
@@ -18,15 +18,33 @@ $app->get('/', function() {
 
 $app->get("/categories/:idcategory", function($idcategory){
 
+	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+
 	$category = new Category();
 
 	$category->get((int)$idcategory);
+
+	$pagination = $category->getProductsPage($page);
+
+	$pages = [];
+
+	for ($i=1; $i <= $pagination['pages']; $i++){
+		array_push($pages, [
+			'link'=>'/categories/'.$category->getidcategory().'?page='.$i,
+			'page'=>$i
+		]);
+	}
+	//var_dump($category->getValues());
+	//var_dump($pagination["data"]);
+	//var_dump($pages);
+	//exit;
 
 	$page = new Page();
 
 	$page->setTpl("category", [
 		'category'=>$category->getValues(),
-		'products'=>Product::checkList($category->getProducts())
+		'products'=>$pagination["data"],
+		'pages'=>$pages
 	]);
 
 });
