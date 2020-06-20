ced9cf5c8149ff819e3732a92336e4416d7dc17f
nyoneyama <nelsonyoneyama@hotmail.com>
2020-06-20 16:36:07 -0300

    site - esqueceu a senha

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -189,7 +189,7 @@ class User extends Model {
 
 	}
 
-	public static function getForgot($email){
+	public static function getForgot($email, $inadmin = true){
 
 		$sql = new Sql();
 
@@ -229,11 +229,16 @@ class User extends Model {
 
 				$code=base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_128, User::SECRET, $dataRecovery["idrecovery"], MCRYPT_MODE_ECB));
 
-				if($inadmin) {
+				if($inadmin === true) {
+
 					$link = "http://www.hcodecommerce.com.br/admin/forgot/reset?code=$code";
+
 				} else {
+
 					$link = "http://www.hcodecommerce.com.br/forgot/reset?code=$code";
+
 				}
+				
 				$mailer = new Mailer($data["desemail"], $data["desperson"], "Redefinir Senha da Hcode Store", "forgot",
 					array(
 					"name"=>$data["desperson"],
